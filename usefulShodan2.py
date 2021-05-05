2429b691388ac533830a6d499225a468d455e0f4
NickSanzotta <nicksanzotta@outlook.com>
2021-05-05 17:59:20 -0400

    Table dev

--- usefulShodan2.py
+++ usefulShodan2.py
@@ -56,13 +56,14 @@ class UsefulShodan():
 			if 'Error: No information available for that IP.' in proc.stderr:
 				return None
 			else:
+				# DEV
 				# Define stdout as lst
 				mylst = proc.stdout.split()
-
 				# Convert lst:strs to lst:tuples ie, (port, protocol).
-				port_protocol = [(mylst[i], mylst[i+1]) for i in range(0, len(mylst), 2)]
-
-				return port_protocol
+				# port_protocol = [(mylst[i], mylst[i+1]) for i in range(0, len(mylst), 2)]
+				# DEV
+				# return port_protocol
+				return mylst[0], mylst[1]
 
 
 	def read_ipaddress(self, filepath):
@@ -98,6 +99,20 @@ class UsefulShodan():
 def main():
 	''' Main func '''
 
+	# DEV: Table
+	from rich.console import Console
+	from rich.table import Table
+	# Table title.
+	table = Table(title="Shodan.io")
+	# Columns defined.
+	table.add_column("IP Address", justify="right", style="cyan", no_wrap=True)
+	table.add_column("Port(s)", style="magenta")
+	table.add_column("Protocol", justify="left", style="green")
+	# Sample row added.
+	table.add_row("127.0.0.1", "445", "DEMO")
+	# Demo Table print.
+	# console.print(table)
+
 	# Const arg value.
 	FILE = sys.argv[1]
 	# Create instance
@@ -108,20 +123,39 @@ def main():
 	ipaddresses = usefulshodan.ipaddresses
 
 	# DEV: verbose flag
-	verbose = True
+	verbose = False
 
 	try:
-		with console.status(f'[txt.spinner]Checking Shodan...') as status:
+		with console.status(f'[txt.spinner]Checking Shodan.io...') as status:
 			for ip in ipaddresses:
 				# Process IP address against Shodan's database.
 				result = usefulshodan.scan_ip(ip)
-				# Print result to stdout.
-				console.log(f'{ip}: {result}')
-				# Non-verbose print that discards results of None type.
-				if not verbose and result is not None:
-					console.log(f'{result}')
+				
+				# DEV - print 
+				# print(type(result))
+				
+				# Non-verbose print, exclude 'None' type results.
+				if verbose == False:
+					if result is not None:
+						console.log(f'{ip}: {result}')
+						# DEV - table
+						table.add_row(f'{ip}', f'{result[0]}', f'{result[1].upper()}')
+				else:
+					# Verbose print, include 'None' type results.
+					console.log(f'{ip}: {result}')
+					if result is not None:
+						# DEV - table
+						table.add_row(f'{ip}', f'{result[0]}', f'{result[1].upper()}')
+					else:
+						# DEV - table
+						table.add_row(f'{ip}', f'{result}', f'{result}')
+
+
 	except KeyboardInterrupt:
 		print(f'\nQuit: detected [CTRL-C]')
+	
+	# DEV
+	console.print(table)
 
 
 if __name__ == '__main__':
