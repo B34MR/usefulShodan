bb5f2c0b88e6e1fb57517789f3ffdb5cadebaa8f
NickSanzotta <nicksanzotta@outlook.com>
2021-05-09 09:55:12 -0400

    Final update to v2

--- usefulShodan2.py
+++ usefulShodan2.py
@@ -3,11 +3,39 @@
 from utils import richard as r
 from utils import sqlite as db
 from utils import shodan
+from utils import arguments
 import sys
 
 
 def main():
 	''' Main func '''
+
+	# Arguments init.
+	args = arguments.parse_args()
+
+	# Args filepath
+	FILE = args.filepath
+
+	# Args drop table.
+	if args.drop:
+		db.drop_table()
+
+	# Args verbose.
+	vverbose = False
+	if args.verbose >=2:
+		vverbose = True
+	verbose = False
+	if args.verbose >=1:
+		verbose = True
+
+	# Database init.
+	db.create_table()
+	# UsefulShoand class instance init.
+	usefulshodan = shodan.UsefulShodan()
+	# Read file and store ipaddresses to instance.
+	usefulshodan.read_file(FILE)
+	# Retrieve ipaddressees from instance.
+	ipaddresses = usefulshodan.ipaddresses
 
 	# Table title.
 	table = r.Table(title="[t.title]UsefulShodan2.py", box=r.box.DOUBLE_EDGE, style='table')
@@ -16,25 +44,10 @@ def main():
 	table.add_column("Port", justify="left", no_wrap=True,  style='t.col2')
 	table.add_column("Protocol", justify="left", no_wrap=True, style='t.col3')
 
-	# User defined filepath
-	FILE = sys.argv[1]
-	# Create instance.
-	usefulshodan = shodan.UsefulShodan()
-	# Read file and store ipaddresses to instance.
-	usefulshodan.read_file(FILE)
-	# Retrieve ipaddressees from instance.
-	ipaddresses = usefulshodan.ipaddresses
-
-	# DEV: verbose flag.
-	verbose = False
-
-	# DEV: debug flag.
-	debug = False
-
 	# Header
-	r.console.print(f'\nUsefulShodan2: [i]An over engineered for-loop for the Shodan-cli.', style='header')
-	r.console.print(f'Shodan.io: [url][link=https://cli.shodan.io/]https://cli.shodan.io', style='header')
-	r.console.print(f'Github.com: [url][link=https://github.com/NickSanzotta/usefulShodan/]https://github.com/NickSanzotta\n', style='header')
+	r.console.print(f'\n[i]UsefulShodan2', style='header')
+	r.console.print(f'[i]An over engineered for-loop for the [url][link=https://cli.shodan.io/]Shodan CLI.', style='header')
+	r.console.print('\n')
 
 	try:
 		for ip in ipaddresses:
@@ -43,29 +56,31 @@ def main():
 				results = usefulshodan.scan_ip(ip)
 				# Unpack results:lst.
 				for result in results:
-					# Non-verbose print, exclude 'None' type results.
-					if verbose == False:
+					# Very-verbose print, include console print and 'None' type results. 
+					if vverbose == True:
+						r.console.log(f'{result}')
+						# Insert ipaddress, port, protocol into database.
+						db.insert_result(result[0], result[1], result[2])
+						# Print Rich Table.
+						table.add_row(f'{result[0]}', f'{result[1]}', f'{result[2]}')
+					
+					# Verbose print, include 'None' type results.
+					elif verbose == True:
+						# Insert ipaddress, port, protocol into database.
+						db.insert_result(result[0], result[1], result[2])
+						# Print Rich Table.
+						table.add_row(f'{result[0]}', f'{result[1]}', f'{result[2]}')
+					
+					# Standard print.
+					else:
 						if result[1] is None:
 							pass
 						else:
-							# DEBUG print
-							# r.console.log(f'[wheat4]{result}')
-
 							# Insert ipaddress, port, protocol into database.
 							db.insert_result(result[0], result[1], result[2])
-
-							# DEV Rich Table
+							# Print Rich Table
 							table.add_row(f'{result[0]}', f'{result[1]}', f'{result[2]}')
-					# Verbose print. include 'None' type results.
-					else:
-						# DEBUG print
-						# r.console.log(f'[wheat4]{result}')
 
-						# Insert ipaddress, port, protocol into database.
-						db.insert_result(result[0], result[1], result[2])
-
-						# Dev Rich Table.
-						table.add_row(f'{result[0]}', f'{result[1]}', f'{result[2]}')
 	except KeyboardInterrupt:
 		print(f'\nQuit: detected [CTRL-C]')
 	
--- utils/sqlite.py
+++ utils/sqlite.py
@@ -8,20 +8,34 @@ conn = sqlite3.connect('database.db')
 # Database cursor.
 c = conn.cursor()
 
-# Database Table init.
-try:
-	with conn:
-		c.execute("""CREATE TABLE Hosts(
-			IP Address text,
-			Port text,
-			Protocol text
-			)""")
-except sqlite3.OperationalError: 
-    pass
+
+def create_table():
+	''' Create table "Hosts" '''
+
+	try:
+		with conn:
+			c.execute("""CREATE TABLE Hosts(
+				IP Address text,
+				Port text,
+				Protocol text
+				)""")
+	except sqlite3.OperationalError: 
+	    pass
+
+
+def drop_table():
+	''' Drop table "Hosts" '''
+	
+	try:
+		with conn:
+			c.execute(f"DROP TABLE Hosts")
+	except sqlite3.OperationalError as e:
+		pass
 
 
 def insert_result(ipaddress, port, protocol):
 	''' Insert result [(IP, Port, Protocol)] '''
+	
 	with conn:
 		c.execute("INSERT INTO Hosts VALUES (:ipaddress, :port, :protocol)",
 		 {'ipaddress': ipaddress, 'port': port, 'protocol': protocol})
